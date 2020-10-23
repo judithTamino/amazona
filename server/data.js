b1a4816f5419e4143eccd6571e7830c17b514457
unknown <taminojudit@gmail.com>
2020-10-23 17:20:35 +0300

    create node server

---
+++ server/data.js
@@ -0,0 +1,35 @@
+export default {
+    products: [
+      {
+        _id: '1',
+        name: "Slim Shirt",
+        category: 'Shirts',
+        image: "/images/d1.jpg",
+        price: 60,
+        brand: "Nike",
+        rating: 4.5,
+        numReviews: 10,
+      },
+      {
+        _id: '2',
+        name: "Fit Shirt",
+        category: 'Shirts',
+        image: "/images/d1.jpg",
+        price: 100,
+        brand: "Nike",
+        rating: 4,
+        numReviews: 356,
+      },
+      {
+        _id: '3',
+        name: "Schino-Slim D Trouser",
+        category: 'Trousers',
+        image: "/images/d1.jpg",
+        price: 40.52,
+        brand: "BOSS",
+        rating: 4.2,
+        numReviews: 100,
+      },
+    ],
+  };
+  
